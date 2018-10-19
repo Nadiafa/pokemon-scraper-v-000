78919ce16704c7d05a37e5faf628878f09a56021
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-19 08:51:49 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
   end 
   
   def self.save(name, type, db)
-    sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type
+    sql = ("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
   
   def find
